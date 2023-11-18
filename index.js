d7b9833ab0c154da4d84b5643aacffc362a03057
alexindevs <alexindevs@gmail.com>
2023-11-18 15:17:54 +0100

    Feat: Added /api/fileanalyze endpoint

--- index.js
+++ index.js
@@ -14,6 +14,32 @@ app.get('/', function (req, res) {
 
 
 
+const multer = require('multer');
+
+
+// Set up multer storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, 'uploads/'); // Specify the directory where files will be stored
+  },
+  filename: function (req, file, cb) {
+      cb(null, file.originalname); // Use the original filename
+  }
+});
+
+const upload = multer({ storage: storage });
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+    // Check if file was provided
+    if (!req.file) {
+        return res.json({ error: 'No file uploaded' });
+    }
+
+      const { originalname, mimetype, size } = req.file;
+      res.json({ name: originalname, type: mimetype, size: size });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
